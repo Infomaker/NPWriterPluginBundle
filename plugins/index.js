51bc4d76acef931acc5aea1044ced4e7287610c6
Danne Lundqvist <danne.lundqvist@infomaker.se>
2016-10-27 17:50:22 +0200

    Load PublishFlow

--- plugins/index.js
+++ plugins/index.js
@@ -1,6 +1,7 @@
 import Skeleton from './se.infomaker.skeleton/index'
 import TextAnalyzer from './se.infomaker.textanalyzer/index'
 import Preamble from './se.infomaker.preamble/Preamble'
+import PublishFlow from './se.infomaker.publishflow/index'
 
 (() => {
     Skeleton()
