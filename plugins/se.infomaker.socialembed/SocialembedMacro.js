3bd73ba8c89cab5de4bf4420ed2523c4c762e720
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-10-28 16:51:23 +0200

    Embed treatment similar to medium

--- plugins/se.infomaker.socialembed/SocialembedMacro.js
+++ plugins/se.infomaker.socialembed/SocialembedMacro.js
@@ -3,14 +3,17 @@ export default {
     execute: function(params, context) {
         let editorSession = context.editorSession
         let text = params.text
+        // only react on 'break' (as medium does)
+        if (!params.action === 'break') return
+        // only react of we find a HTTP URL
         let match = /^\s*(https?:\/\/([^\s]+))\s*$/.exec(text)
-        if (match) {
-            let url = match[1]
-            editorSession.selectNode(params.node.id)
-            editorSession.executeCommand('socialembed', {
-                url: url
-            })
-            return true
-        }
+        if (!match) return
+        // take the url, select the node, and run the social embed command
+        let url = match[1]
+        editorSession.selectNode(params.node.id)
+        editorSession.executeCommand('socialembed', {
+            url: url
+        })
+        return true
     }
 }
