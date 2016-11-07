8a4c16269111001ca9234ec286885233a23f6eaf
Michael Aufreiter <michael@substance.io>
2016-11-07 13:43:27 +0100

    Create stubs for ImageCropper and ImageMetadata dialogs.

---
+++ plugins/se.infomaker.ximimage/ImageCropper.js
@@ -0,0 +1,16 @@
+import { Component } from 'substance'
+
+/*
+  Used in ImageDisplay
+*/
+class ImageCropper extends Component {
+    render($$) {
+        let el = $$('div').addClass('sc-image-cropper')
+
+        el.append('TODO: Implement image cropper here')
+        return el
+    }
+}
+
+export default ImageCropper
+
--- plugins/se.infomaker.ximimage/ImageDisplay.js
+++ plugins/se.infomaker.ximimage/ImageDisplay.js
@@ -1,14 +1,25 @@
 import { Component } from 'substance'
 
+import ImageCropper from './ImageCropper'
+import ImageMetadata from './ImageMetadata'
+
 /*
   Intended to be used in Ximimage and Ximteaser
 */
 class ImageDisplay extends Component {
+    didMount() {
+        this.handleActions({
+            closeModal: this._closeDialog
+        })
+    }
+
     render($$) {
         let node = this.props.node
         let el = $$('div').addClass('sc-image-display')
         let imgSrc = node.getUrl()
         let Button = this.getComponent('button')
+        let Modal = this.getComponent('modal')
+        let DialogClass = this.state.DialogClass
 
         if (imgSrc) {
             el.append(
@@ -37,6 +48,20 @@ class ImageDisplay extends Component {
                 }).on('click', this._openCropper)
             )
         )
+
+        // Render dialog if open
+        if (DialogClass) {
+            el.append(
+                $$(Modal, {
+                    width: 'medium',
+                    textAlign: 'center'
+                }).append(
+                    $$(DialogClass, {
+                        node: node
+                    })
+                )
+            )
+        }
         return el
     }
 
@@ -60,12 +85,22 @@ class ImageDisplay extends Component {
         })
     }
 
+    _closeDialog() {
+        this.setState({
+            DialogClass: null
+        })
+    }
+
     _openMetaData() {
-        this.context.editorSession.startWorkflow('edit-image-metadata', {node: this.props.node})
+        this.setState({
+            DialogClass: ImageMetadata
+        })
     }
 
     _openCropper() {
-        this.context.editorSession.startWorkflow('crop-image', {node: this.props.node})
+        this.setState({
+            DialogClass: ImageCropper
+        })
     }
 }
 
