65ca6b40588dacecdf3dad9c8ca8d66f681537a0
Tobias Södergren <tobias.sodergren@infomaker.se>
2016-11-10 13:28:48 +0100

    Work work work

--- plugins/se.infomaker.ximplace/LocationItemComponent.js
+++ plugins/se.infomaker.ximplace/LocationItemComponent.js
@@ -1,22 +1,22 @@
 'use strict';
 
-var Component = require('substance/ui/Component');
-var $$ = Component.$$;
-var Icon = require('substance/ui/FontAwesomeIcon');
-var jxon = require('jxon/index');
+import {Component} from 'substance'
+// var $$ = Component.$$;
+import {Icon} from 'substance'
+import {jxon} from 'writer'
+import {isArray} from 'lodash'
 
-function LocationItemComponent() {
-    LocationItemComponent.super.apply(this, arguments);
-    this.name = 'location';
-}
 
-LocationItemComponent.Prototype = function () {
+class LocationItemComponent extends Component {
+    constructor(...args) {
+        super(args)
+    }
 
-    this.loadLocation = function () {
-        this.ajaxRequest = this.context.api.router.ajax('GET', 'xml', '/api/newsitem/' + this.props.location.uuid, {imType: this.props.location.type});
+    loadLocation() {
+        this.ajaxRequest = this.context.api.router.ajax('GET', 'xml', '/api/newsitem/' + this.props.location.uuid, {imType: this.props.location.type})
         this.ajaxRequest
             .done(function (data) {
-                var conceptXML = data.querySelector('conceptItem');
+                var conceptXML = data.querySelector('conceptItem')
                 this.setState({
                     loadedLocation: jxon.build(conceptXML),
                     isLoaded: true
@@ -27,39 +27,38 @@ LocationItemComponent.Prototype = function () {
                     isLoaded: true,
                     couldNotLoad: true
                 });
-            }.bind(this));
-    };
+            }.bind(this))
+    }
 
+    render($$) {
 
-    this.render = function () {
+        var location = this.props.location
 
-        var location = this.props.location;
+        var locItem = $$('li').addClass('tag-list__item')
+        var displayNameEl = $$('span')
 
-        var locItem = $$('li').addClass('tag-list__item');
-        var displayNameEl = $$('span');
-
-        var locationType = 'position';
+        var locationType = 'position'
 
         if (!this.state.isLoaded) {
-            displayNameEl.addClass('tag-item__title tag-item__title--loading').append(location.title);
+            displayNameEl.addClass('tag-item__title tag-item__title--loading').append(location.title)
             this.loadLocation();
 
         } else {
             if (this.state.couldNotLoad) {
                 displayNameEl.addClass('tag-item__title tag-item__title--no-avatar tag-item__title--notexisting')
                     .append(location.title)
-                    .attr('title', this.context.i18n.t('This item could not be loaded. UUID: ')+location.uuid);
+                    .attr('title', this.context.i18n.t('This item could not be loaded. UUID: ') + location.uuid)
             } else {
-                locationType = this.state.loadedLocation.concept.metadata.object['@type'];
-                displayNameEl.addClass('tag-item__title tag-item__title--no-avatar').append(this.state.loadedLocation.concept.name);
+                locationType = this.state.loadedLocation.concept.metadata.object['@type']
+                displayNameEl.addClass('tag-item__title tag-item__title--no-avatar').append(this.state.loadedLocation.concept.name)
 
-                displayNameEl.attr('title', location.title);
+                displayNameEl.attr('title', location.title)
 
-                this.updateTagItemName(displayNameEl, this.state.loadedLocation);
+                this.updateTagItemName(displayNameEl, this.state.loadedLocation)
 
                 displayNameEl.attr('data-toggle', 'tooltip')
                     .attr('data-placement', 'bottom')
-                    .attr('data-trigger', 'manual');
+                    .attr('data-trigger', 'manual')
 
 
                 // TODO: Find a way to update the NewsItem when a concept name has changed
@@ -71,74 +70,76 @@ LocationItemComponent.Prototype = function () {
                 }
 
                 displayNameEl.on('click', function () {
-                    this.props.openMap(this.state.loadedLocation);
-                }.bind(this));
+                    this.props.openMap(this.state.loadedLocation)
+                }.bind(this))
 
-                displayNameEl.on('mouseenter', this.toggleTooltip);
-                displayNameEl.on('mouseout', this.hideTooltip);
+                // displayNameEl.on('mouseenter', this.toggleTooltip)
+                // displayNameEl.on('mouseout', this.hideTooltip)
 
             }
 
-            locItem.append(displayNameEl);
+            locItem.append(displayNameEl)
 
             var deleteButton = $$('span').append($$(Icon, {icon: 'fa-times'})
                 .addClass('tag-icon tag-icon--delete')
                 .attr('title', this.context.i18n.t('Remove from article')))
                 .on('click', function () {
-                    this.removeTag(location);
-                }.bind(this));
-            locItem.append(deleteButton);
+                    this.removeTag(location)
+                }.bind(this))
+            locItem.append(deleteButton)
 
-            var icon = 'fa-map-marker';
+            var icon = 'fa-map-marker'
             if (locationType === 'x-im/polygon') {
-                icon = 'fa-map';
+                icon = 'fa-map'
             }
-            locItem.append($$(Icon, {icon: icon}).addClass('tag-icon'));
+            locItem.append($$(Icon, {icon: icon}).addClass('tag-icon'))
         }
 
 
         return locItem;
-    };
+    }
 
-    this.removeTag = function (location) {
+
+    removeTag(location) {
         this.$el.first().fadeOut(200, function () {
-            this.props.removeLocation(location);
-        }.bind(this));
+            this.props.removeLocation(location)
+        }.bind(this))
 
-    };
+    }
 
-    this.toggleTooltip = function (ev) {
-        $(ev.target).tooltip('toggle');
-        ev.target.timeout = window.setTimeout(function () {
-            this.hideTooltip(ev)
-        }.bind(this), 3000)
-    };
+    // toggleTooltip(ev) {
+    //     $(ev.target).tooltip('toggle')
+    //     ev.target.timeout = window.setTimeout(function () {
+    //         this.hideTooltip(ev)
+    //     }.bind(this), 3000)
+    // }
+    //
+    // hideTooltip(ev) {
+    //     if (ev.target.timeout) {
+    //         window.clearTimeout(ev.target.timeout);
+    //         ev.target.timeout = undefined;
+    //     }
+    //     $(ev.target).tooltip('hide')
+    // }
 
-    this.hideTooltip = function (ev) {
-        if (ev.target.timeout) {
-            window.clearTimeout(ev.target.timeout);
-            ev.target.timeout = undefined;
-        }
-        $(ev.target).tooltip('hide');
-    };
 
-
-    this.updateTagItemName = function (tagItem, loadedTag) {
+    updateTagItemName(tagItem, loadedTag) {
         if (loadedTag.concept && loadedTag.concept.definition) {
-            var definition = _.isArray(loadedTag.concept.definition) ? loadedTag.concept.definition : [loadedTag.concept.definition];
+            var definition = isArray(loadedTag.concept.definition) ? loadedTag.concept.definition : [loadedTag.concept.definition]
             for (var i = 0; i < definition.length; i++) {
-                var item = definition[i];
+                var item = definition[i]
                 if (item["@role"] === "drol:short") {
                     if (item["keyValue"] && item["keyValue"].length > 0) {
-                        tagItem.attr('title', item["keyValue"]);
-                        break;
+                        tagItem.attr('title', item["keyValue"])
+                        break
                     }
                 }
             }
         }
-    };
+    }
 
-};
 
-Component.extend(LocationItemComponent);
-module.exports = LocationItemComponent;
+}
+
+export default LocationItemComponent
+
