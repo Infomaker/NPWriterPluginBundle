c1bfff1446ad19f7179aede32e3f4214922cf398
Michael Aufreiter <michael@substance.io>
2016-11-14 16:33:19 +0100

    Improve insertImage.

--- plugins/se.infomaker.ximimage/insertImage.js
+++ plugins/se.infomaker.ximimage/insertImage.js
@@ -5,16 +5,28 @@
     data is a file or uri and calls the appropriate sync(=upload) method.
 */
 export default function(tx, data) {
+    let isFile = data instanceof File
+    let mimeType
+
+    if (isFile) {
+        mimeType = data.type
+    } else if (typeof data === 'string') {
+        let ext = data.split('.').pop()
+        // Extract use file extension
+        mimeType = 'image/'+ext
+    } else {
+        throw new Error('Unsupported data. Must be File or String')
+    }
 
     // Create file node for the image
     let imageFile = tx.create({
         type: 'npfile',
         fileType: 'image',
-        mimeType: file.type,
+        mimeType: mimeType,
         data: data, // either File object or uri
         caption: 'Enter caption here',
         // QUESTION Michael: why is this needed?
-        knownData: Boolean(file)
+        knownData: isFile
     })
 
     // Inserts image at current cursor pos
