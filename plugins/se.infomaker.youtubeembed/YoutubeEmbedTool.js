fa4e6c43b64d1beab9aa185fa8ede6b508bb0c00
Ola Andersson <ola.andersson@infomaker.se>
2016-11-15 20:26:43 +0100

    Fix styling of youtube button

--- plugins/se.infomaker.youtubeembed/YoutubeEmbedTool.js
+++ plugins/se.infomaker.youtubeembed/YoutubeEmbedTool.js
@@ -3,15 +3,17 @@ import {Tool, FontAwesomeIcon} from 'substance'
 class YoutubeEmbedTool extends Tool {
 
     render($$) {
-        const el = $$('span')
+        const el = $$('div')
         const youtubeIcon = $$(FontAwesomeIcon, {icon: 'fa-youtube'})
         youtubeIcon.on('click', () => {
             this.getCommandName()
             this.executeCommand()
         })
 
+        const btn = $$('button').addClass('se-tool')
+        btn.append(youtubeIcon)
 
-        el.append(youtubeIcon)
+        el.append(btn)
 
         return el
     }
