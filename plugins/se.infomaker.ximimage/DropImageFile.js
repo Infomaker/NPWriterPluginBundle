a37bb4ab02d0784f2c2beda330ea79d6cb2d6b7a
Ola Andersson <ola.andersson@infomaker.se>
2016-11-15 10:27:33 +0100

    Fixed erronous import statement

--- plugins/se.infomaker.ximimage/DropImageFile.js
+++ plugins/se.infomaker.ximimage/DropImageFile.js
@@ -1,11 +1,11 @@
-import startsWith from 'lodash/startsWith'
+import lodash from 'writer'
 import insertImage from './insertImage'
 import { DragAndDropHandler } from 'substance'
 
 // Implements a file drop handler
 class DropImageFile extends DragAndDropHandler {
     match(params) {
-        return params.type === 'file' && startsWith(params.file.type, 'image')
+        return params.type === 'file' && lodash.startsWith(params.file.type, 'image')
     }
 
     drop(tx, params) {
