d80ffad737ab823852d87af2c565135d5f0a5cc0
Andreas Kihlberg <andreas.kihlberg@infomaker.se>
2016-11-23 13:42:55 +0100

    Update webpack for production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -47,7 +47,7 @@ module.exports = {
                 test: /\.js?$/,
                 exclude: /(node_modules)/,
                 loaders: [
-                    'babel?presets[]=stage-0,presets[]=es2015'
+                    'babel?presets[]=stage-0,presets[]=es2015-node6'
                 ]
             }
         ],
