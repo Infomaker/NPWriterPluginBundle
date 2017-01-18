a223cc54fb40ffbf30c55b5977aabf42f2381553
Ola Andersson <ola.andersson@infomaker.se>
2017-01-18 11:43:45 +0100

    Added dummy images

--- plugins/se.infomaker.newspilot.job/JobComponent.js
+++ plugins/se.infomaker.newspilot.job/JobComponent.js
@@ -17,11 +17,11 @@ class JobComponent extends Component {
                 },
                 {
                     name: "Cthulhu B.jpg",
-                    url: "http://vignette3.wikia.nocookie.net/lovecraft/images/6/67/Cthulhu.jpg/revision/latest/zoom-crop/width/240/height/240?cb=20140818055533"
+                    url: "http://vignette3.wikia.nocookie.net/lovecraft/images/9/9a/HickmanCthulhu.jpg"
                 },
                 {
-                    name: "Cthulhu B.jpg",
-                    url: "http://vignette3.wikia.nocookie.net/lovecraft/images/6/67/Cthulhu.jpg/revision/latest/zoom-crop/width/240/height/240?cb=20140818055533"
+                    name: "Cthulhu C.jpg",
+                    url: "http://img05.deviantart.net/067d/i/2014/177/6/2/cthulhu_by_glooh-d7o0g9p.jpg"
                 }
             ]
         }
@@ -38,7 +38,7 @@ class JobComponent extends Component {
             jobImages: this.state.jobImages
         }).ref('imageList')
 
-        el.append($$('h2').append(this.getLabel('Bilder i Newspilot jobb')))
+        el.append($$('h2').append(this.getLabel('Bilder')))
         el.append(imageList)
 
         return el;
