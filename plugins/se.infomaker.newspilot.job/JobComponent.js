6c3d0210c660441d29373d97371dc103181f1b47
Tobias Södergren <tobias.sodergren@infomaker.se>
2017-01-18 11:06:16 +0100

    WPLUG-2: Testing with a link to an image

--- plugins/se.infomaker.newspilot.job/JobComponent.js
+++ plugins/se.infomaker.newspilot.job/JobComponent.js
@@ -17,8 +17,22 @@ class JobComponent extends Component {
         }
     }
 
+    _onDragStart(e) {
+        e.stopPropagation()
+    }
+
     render($$) {
         const el = $$('div').addClass('jobimages')
+
+        const dummyLink = $$('a')
+            .attr('href', 'https://cdn.pixabay.com/photo/2017/01/06/20/43/soap-bubble-1958841_640.jpg')
+            .attr('draggable', true)
+            .on('click', function (evt) {
+                evt.stopPropagation();
+            })
+            .on('dragstart', this._onDragStart, this)
+            .append("Hello image")
+
 
         const imageList = $$(JobImagesListComponent, {
             jobImages: this.state.jobImages
@@ -26,6 +40,7 @@ class JobComponent extends Component {
 
         el.append($$('h2').append(this.getLabel('Newspilot job images loaded!')))
         el.append(imageList)
+        el.append(dummyLink)
 
         return el;
     }
