8e25499f735386f4e555392f25ed10e2673fa7f2
Tobias Södergren <tobias.sodergren@infomaker.se>
2017-01-30 11:50:09 +0100

    Testing with existing url

--- plugins/se.infomaker.newspilot.job/JobComponent.js
+++ plugins/se.infomaker.newspilot.job/JobComponent.js
@@ -84,7 +84,7 @@ class JobComponent extends Component {
 
 
     getNewspilotLoginUrl() {
-        return this.getNewspilotServer() + '/newspilot/rest/'
+        return this.getNewspilotServer() + '/newspilot/rest/sessions'
     }
 
     getNewspilotServer() {
