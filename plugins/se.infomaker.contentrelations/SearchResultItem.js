1ea4bf08acee1dcebd7ae35aa59692c337d9f215
Ola Andersson <ola.andersson@infomaker.se>
2017-03-03 13:21:28 +0100

    WPLUG-32

--- plugins/se.infomaker.contentrelations/SearchResultItem.js
+++ plugins/se.infomaker.contentrelations/SearchResultItem.js
@@ -10,8 +10,18 @@ class SearchResultItem extends Component {
         }
     }
 
-    getDroplinkForItem(item) {
+    didMount() {
+        this.dropLink = this._getDroplinkForItem(this.props.item)
+    }
 
+    render($$) {
+        const item = this.props.item
+        const imType = item.imType ? item.imType[0] : 'article'
+
+        return this._getItem($$, item, imType)
+    }
+
+    _getDroplinkForItem(item) {
         const imType = item.imType[0]
         const name = item.name[0]
 
@@ -32,40 +42,14 @@ class SearchResultItem extends Component {
         }
     }
 
-    getTitle($$, item) {
-
-        let name = item.name ? item.name[0] : "Unidentified",
-            products = item.type,
-            productsType = item.typeCatalog ? item.typeCatalog[0] : 'text'
-
-        if (!products) {
-            return name;
-        }
-
-        if (productsType && productsType === 'icon') {
-            const icons = products.map(function (product) {
-                return $$('img').attr('src', product).addClass('product-icon');
-            });
-
-            const result = $$('span').append(name);
-
-            icons.forEach(function (icon) {
-                result.append(icon)
-            });
-
-            return result;
-        } else {
-            return `${name} [${products.join(', ')}]`;
-        }
-    }
-
-    getProducts($$, item) {
+    _getProducts($$, item) {
         let products = item.type,
             productsType = item.typeCatalog ? item.typeCatalog[0] : 'text'
 
-        let result = $$('span')
+        let result = $$('div')
+            .addClass('icondiv')
 
-        if (productsType && productsType === 'icon') {
+        if (productsType && productsType === 'icon' && products) {
             const icons = products.map(function (product) {
                 return $$('img').attr('src', product).addClass('product-icon');
             });
@@ -73,16 +57,11 @@ class SearchResultItem extends Component {
             icons.forEach(function (icon) {
                 result.append(icon)
             });
-
-            return result;
-        } else {
-            result.append('The fox')
+        } else if (products) {
+            result.append(products.join(', '))
         }
-    }
 
-
-    didMount() {
-        this.dropLink = this.getDroplinkForItem(this.props.item)
+        return result;
     }
 
     _onDragStart(e) {
@@ -90,34 +69,7 @@ class SearchResultItem extends Component {
         e.dataTransfer.setData('text/uri-list', this.dropLink)
     }
 
-    render($$) {
-        const item = this.props.item
-
-        return this.getItem($$, item)
-    }
-
-    getItem($$, item) {
-        const imType = item.imType ? item.imType[0] : 'article'
-
-        console.log('SERVICES: ', item.type)
-
-        switch (imType) {
-            case 'article':
-                return this.getArticleItem($$, item)
-            case 'image':
-                return this.getImageItem($$, item)
-            default:
-
-        }
-    }
-
-
-    /**
-     * Get a list item for an article
-     * @param $$
-     * @param item
-     */
-    getArticleItem($$, item) {
+    _getItem($$, item, type) {
         const listItem = $$('li')
             .addClass('box')
 
@@ -129,152 +81,69 @@ class SearchResultItem extends Component {
 
         const divContent = $$('div')
             .addClass('content')
-
-        // Header
-        // const icon = $$(FontAwesomeIcon, {icon: 'fa-file-text-o'}).addClass('type-icon')
-
-        // const header = $$('header')
-        //     .addClass('header')
-        //     .append(icon)
-
-        // Main
-        const icon = $$(FontAwesomeIcon, {icon: 'fa-file-text-o'}).addClass('type-icon')
-        const mainIconDiv = $$('div')
-            .addClass('icondiv')
-            .append(icon)
-
-        const name = item.name ? item.name[0] : "Unidentified"
-        const mainNameDiv = $$('div')
-            .addClass('namediv')
-            .append(name)
 
         const main = $$('div')
             .addClass('main')
-            .append([mainIconDiv, mainNameDiv])
 
-        // Footer
-        const footer = $$('footer')
-            .addClass('footer')
-            .append(this.getProducts($$, item))
-
-        divContent.append([main, footer])
-
-        listItem.append([divDrag, divContent])
-
-        return listItem
-
-        // const listItem = $$('li')
-        //     .addClass('article')
-        //     .attr('draggable', true)
-        //     .on('dragstart', this._onDragStart, this)
-        //
-        // const icon = $$(FontAwesomeIcon, {icon: 'fa-file-text-o'}).addClass('type-icon')
-        // const label = $$('span').addClass('title').append(this.getTitle($$, item))
-        //
-        // listItem.append([icon, label])
-        // return listItem
-    }
-
-
-    /**
-     * Get a list item for an image
-     * @param $$
-     * @param item
-     */
-    getImageItem($$, item) {
-
-        const listItem = $$('li')
-            .addClass('box')
-
-        const divDrag = $$('div')
-            .addClass('drag')
-            .attr('draggable', true)
-            .on('dragstart', this._onDragStart, this)
-            .html(this._getSvg())
-
-        const divContent = $$('div')
-            .addClass('content')
-
-        // Header
-        // const icon = $$(FontAwesomeIcon, {icon: 'fa-picture-o'}).addClass('type-icon')
-        //
-        // const header = $$('header')
-        //     .addClass('header')
-        //     .append(icon)
-
-        // Main
-        const icon = $$(FontAwesomeIcon, {icon: 'fa-picture-o'}).addClass('type-icon')
+        // Icon
+        const icon = $$(FontAwesomeIcon, {icon: this._getIcon(type)}).addClass('type-icon')
         const mainIconDiv = $$('div')
             .addClass('icondiv')
             .append(icon)
 
+        // Name
         const name = item.name ? item.name[0] : "Unidentified"
         const mainNameDiv = $$('div')
             .addClass('namediv')
             .append(name)
 
-        let image
+        // Products
+        const products = this._getProducts($$, item)
 
-        if (this.state.imageLoaded) {
-            image = $$('img').attr('src', this.state.imageURL).addClass('image-icon')
-        } else {
-            this.fetchImageURLForUUID(item.uuid)
-                .then(url => {
-                    this.setState({
-                        imageLoaded: true,
-                        imageURL: url
+        // Media
+        const media = this._getMedia($$, item, type)
+
+        // Go append!
+        main.append([mainIconDiv, mainNameDiv, products, media])
+        divContent.append(main)
+        listItem.append([divDrag, divContent])
+
+        return listItem
+    }
+
+    _getMedia($$, item, type) {
+        if ('image' === type) {
+            let image
+            if (this.state.imageLoaded) {
+                image = $$('img').attr('src', this.state.imageURL).addClass('image-icon')
+            } else {
+                this._fetchImageURLForUUID(item.uuid)
+                    .then(url => {
+                        this.setState({
+                            imageLoaded: true,
+                            imageURL: url
+                        })
                     })
-                })
+            }
+
+            const mainImageDiv = $$('div')
+                .addClass('imagediv')
+                .append(image)
+
+            return mainImageDiv
         }
-
-        const mainImageDiv = $$('div')
-            .addClass('imagediv')
-            .append(image)
-
-        const main = $$('div')
-            .addClass('main')
-            .append([mainIconDiv, mainNameDiv, mainImageDiv])
-
-        // Footer
-        const footer = $$('footer')
-            .addClass('footer')
-
-        divContent.append([main, footer])
-
-        listItem.append([divDrag, divContent])
-
-        return listItem
-        // const listItem = $$('li')
-        //     .addClass('image')
-        //     .attr('draggable', true)
-        //     .on('dragstart', this._onDragStart, this)
-        //
-        // let icon = $$(FontAwesomeIcon, {icon: 'fa-picture-o'})
-        // const label = $$('span').addClass('title').append(this.getTitle($$, item))
-        //
-        // if (this.state.imageLoaded) {
-        //     icon = $$('img').attr('src', this.state.imageURL).addClass('image-icon')
-        // } else {
-        //     this.fetchImageURLForUUID(item.uuid)
-        //         .then(url => {
-        //             this.setState({
-        //                 imageLoaded: true,
-        //                 imageURL: url
-        //             })
-        //         })
-        // }
-        //
-        // listItem.append([icon, label])
-        // return listItem
     }
 
+    _getIcon(type) {
+        return type === 'image' ? 'fa-picture-o' : 'fa-file-text-o'
+    }
 
     /**
      * Fetch and url for an image via the backend
      * @param uuid
      * @returns {Promise.<T>|*|Promise}
      */
-    fetchImageURLForUUID(uuid) {
+    _fetchImageURLForUUID(uuid) {
         return api.router.get('/api/binary/url/' + uuid + '/40?imType=x-im/image&height=40')
 
             .then(response => response.text())
@@ -289,4 +158,5 @@ class SearchResultItem extends Component {
     }
 
 }
+
 export default SearchResultItem
