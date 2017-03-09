e91af279d1efa4b656f6ea5c105f1c99c64eacfb
Andreas Kihlberg <andreas.kihlberg@infomaker.se>
2017-03-09 16:22:58 +0100

    Update on history plugin

--- plugins/se.infomaker.history/HistoryMainComponent.js
+++ plugins/se.infomaker.history/HistoryMainComponent.js
@@ -28,79 +28,15 @@ class HistoryMainComponent extends Component {
     }
 
 
-    didMount() {
-        // var historyArticles = api.history.getHistory();
-        //
-        // var unsavedArticles = historyArticles.filter(function (history) {
-        //     return history.unsavedArticle === true;
-        // })
-        //
-        // // If we already have loaded an article from history we should not open the dialog again.
-        // let historyForArticle = api.history.get(api.newsItem.getIdForArticle());
-        //
-        // if (historyForArticle && historyForArticle.versions && historyForArticle.versions.length > 0 && api.newsItem.hasTemporaryId()) {
-        //     return
-        // }
-        // else if (!api.newsItem.hasTemporaryId() && historyForArticle.versions.length === 0) {
-        //     return
-        // }
-        // else if (unsavedArticles.length === 0) {
-        //     return
-        // }
-        //
-        //
-        // let title = this.getLabel('Unsaved articles found')
-        // let description = this.getLabel('It looks like there are one or more unsaved articles. Do you want to restore an unsaved article?')
-        // let primaryButton = this.getLabel('No thanks, create new article')
-        // let secondaryButton = this.getLabel('Restore latest unsaved article')
-        // let existingArticle = false
-        //
-        // if (!api.newsItem.hasTemporaryId() && historyForArticle.versions.length > 0) { // If we load a existing article
-        //     unsavedArticles = [historyForArticle];
-        //     existingArticle = true
-        //
-        //     title = this.getLabel('Unsaved changes found for this article')
-        //     description = this.getLabel('We found some unsaved changes for this article. Do you want to restore the unsaved changes?')
-        //     primaryButton = this.getLabel('Restore unsaved changes')
-        //     secondaryButton = this.getLabel('No thanks, just open the article')
-        // }
-        //
-        // if(api.document.getDocumentStatus() === 'writerHasNoDocument') {
-        //     api.ui.showDialog(
-        //         VersionSelectorDialog,
-        //         {
-        //             existingArticle: existingArticle,
-        //             unsavedArticles: unsavedArticles,
-        //             descriptionText: description,
-        //             applyVersion: this.applyVersion.bind(this)
-        //         },
-        //         {
-        //             global: true,
-        //             title: title,
-        //             primary: primaryButton,
-        //             secondary: secondaryButton
-        //         }
-        //     )
-        // }
-
-    }
-
 
     updateHistoryState() {
-
-        let id = this.context.api.newsItem.getIdForArticle()
-
-        this.setState({
-            historyForArticle: this.context.api.history.get(id),
+        this.extendState({
             historyArticles: this.context.api.history.getHistory()
         });
     }
 
     getInitialState() {
-        let id = this.context.api.newsItem.getIdForArticle()
-
         return {
-            historyForArticle: this.context.api.history.get(id),
             historyArticles: this.context.api.history.getHistory()
         }
     }
@@ -108,7 +44,7 @@ class HistoryMainComponent extends Component {
 
     render($$) {
 
-        const el = $$('div').addClass('imc-history light').append(
+        const el = $$('div').ref('historyContainer').addClass('imc-history light').append(
             $$('h2').append(this.getLabel('history-popover-headline'))
         )
         el.append($$('p').append(this.getLabel('history-popover-description')))
@@ -122,7 +58,7 @@ class HistoryMainComponent extends Component {
         const scrollpane = api.ui.getComponent('scroll-pane')
         const scroll = $$(scrollpane, {
             scrollbarType: 'native'
-        })
+        }).ref('historyScroll')
 
 
         let versions = this.state.historyArticles.map(function (article) {
@@ -147,7 +83,7 @@ class HistoryMainComponent extends Component {
             article: article,
             applyVersion: this.applyVersion.bind(this),
             removeArticle: this.removeArticle.bind(this)
-        })
+        }).ref(article.id)
     }
 
     removeAll() {
