9b61f5c1a8dae4c8dc97da60bd2c1ed63fd58990
Danne Lundqvist <danne@dotvoid.se>
2017-05-18 15:57:20 +0200

    Refactored code, wait for transaction to be finished (idle) before proceeding

--- plugins/se.infomaker.ximimage/DropImageFile.js
+++ plugins/se.infomaker.ximimage/DropImageFile.js
@@ -10,30 +10,55 @@ class DropImageFile extends DragAndDropHandler {
 
     drop(tx, params) {
         const nodeId = insertImage(tx, params.file)
-        setTimeout(() => {
-            api.editorSession.fileManager.sync()
-                .catch((err) => {
-                    // TODO When image cannot be uploaded, the proxy, file node and object node should be removed using the api.
-                    let errors = [err.message]
-                    try {
-                        const document = api.editorSession.getDocument()
-                        const node = document.get(nodeId),
-                            imageFile = node.imageFile
-                        api.document.deleteNode('ximimage', node)
-                        if (imageFile) {
-                            api.editorSession.transaction((tx) => {
-                                tx.delete(imageFile)
-                            })
-                        }
-                    }
-                    catch(e) {
-                        errors.push(e.message)
-                    }
-                    api.ui.showNotification('ximimage', 'Error', errors.join(' - '))
+        const maxIterations = 500
 
+        let iterations = 0
+        let intervalId = setInterval(() => {
+            if (tx._state === 'idle') {
+                clearInterval(intervalId)
+                this.sync(tx, nodeId)
+            }
+            else if (iterations++ > maxIterations) {
+                clearInterval(intervalId)
+                this.showErrorNotification(tx, nodeId, 'Failed adding image due to internal transaction state not finishing')
+            }
+        }, 50)
+    }
+
+
+    sync(tx, nodeId) {
+        api.editorSession.fileManager.sync().catch((err) => {
+            this.showErrorNotification(tx, nodeId, err.message)
+        })
+    }
+
+    showErrorNotification(tx, nodeId, errorMessage) {
+        let errors = [errorMessage]
+        this.removeNodesOnUploadFailure(tx, nodeId, errors)
+
+        api.ui.showNotification('ximimage', 'Error', errors.join(' - '))
+    }
+
+    /**
+     * @todo When image cannot be uploaded, the proxy, file node and object node should be removed using the api.
+     */
+    removeNodesOnUploadFailure(tx, nodeId, errors) {
+        try {
+            const document = api.editorSession.getDocument(),
+                node = document.get(nodeId)
+
+            api.document.deleteNode('ximimage', node)
+
+            const imageFile = node.imageFile
+            if (imageFile) {
+                api.editorSession.transaction((tx) => {
+                    tx.delete(imageFile)
                 })
-
-        }, 300)
+            }
+        }
+        catch(e) {
+            errors.push(e.message)
+        }
     }
 }
 
