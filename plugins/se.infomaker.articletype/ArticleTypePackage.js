5d99626b6002d4900fc7004429f77d1424a4308a
Ola Andersson <olaanders@gmail.com>
2017-05-29 15:50:44 +0200

    WPLUG-86

---
+++ plugins/se.infomaker.articletype/ArticleTypePackage.js
@@ -0,0 +1,18 @@
+import './scss/articletype.scss';
+
+import ArticleTypeComponent from './ArticleTypeComponent'
+
+export default {
+    id: 'se.infomaker.articletype',
+    name: 'articletype',
+    version: '{{version}}',
+    configure: function (config) {
+
+        config.addLabel('articletype-label', {
+            en: 'Article type',
+            sv: 'Artikeltyp'
+        });
+
+        config.addComponentToSidebarWithTabId(this.id, 'main', ArticleTypeComponent)
+    }
+}
