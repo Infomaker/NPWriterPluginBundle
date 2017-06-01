5dc0a20a30c2a634d2dd009cbfb7920b83c46f2a
Danne Lundqvist <danne@dotvoid.se>
2017-06-01 13:41:48 +0200

    WPLUG-77: WiP adding fields to image meta dialog via extension

--- plugins/se.infomaker.imageextension/ImageExtensionComponent.js
+++ plugins/se.infomaker.imageextension/ImageExtensionComponent.js
@@ -1,8 +1,58 @@
 import {Component} from 'substance'
 
 class ImageExtension extends Component {
+    constructor(...args) {
+        super(...args)
+
+        this.fields = {
+            gender: 'Gender bias in image',
+            type: 'Describe the main objects in the image'
+        }
+    }
+
     render($$) {
-        return $$('div').append('wooot!')
+        const el = $$('div')
+        for (var key in this.fields) {
+            let value = ''
+            if (this.props.properties && typeof this.props.properties[key] !== 'undefined') {
+                value = this.props.properties[key]
+            }
+
+            el.append(
+                this.renderInputField(
+                    $$,
+                    key,
+                    this.fields[key],
+                    value
+                )
+            )
+        }
+
+        return el
+    }
+
+    renderInputField($$, id, name, value) {
+        return $$('div')
+            .addClass('form-group flexible-label')
+            .append (
+                $$('input')
+                    .on('change', () => {
+                        // FIXME: Handle change via props callback
+                    })
+                    .addClass('form-control')
+                    .attr({
+                        required: 'required',
+                        type: 'text',
+                        id: 'dialog-image-info-' + id,
+                        placeholder: this.getLabel(name)
+                    })
+                    .val(value),
+                $$('label')
+                    .attr('for', 'dialog-image-info-' + id)
+                    .append(
+                        this.getLabel(name)
+                    )
+            )
     }
 }
 
