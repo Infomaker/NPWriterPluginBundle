51321a61e97e63ab94a1370fd43bf5459f46d5ce
Christoffer Ekberg <christoffer.ekberg@infomaker.se>
2017-10-06 16:20:56 +0200

    WPLUG-121: Create model for allowed image types and move isImage logic to ImageTypes::containsExtension

--- plugins/se.infomaker.ximimage/models/isImage.js
+++ plugins/se.infomaker.ximimage/models/isImage.js
@@ -1,15 +1,5 @@
+import {containsExtension} from './ImageTypes'
+
 export default (uri, api) => {
-
-    // Load allowed filextension from config file
-    let fileExtensionsFromConfig = api.getConfigValue('se.infomaker.ximimage', 'imageFileExtension')
-
-    //If no extension specified in config use the default extension, specified in constructor
-    if (!fileExtensionsFromConfig) {
-        fileExtensionsFromConfig = ['.jpg', '.jpeg', '.png', '.gif']
-    }
-    // Iterate given extension and return bool if found
-    return fileExtensionsFromConfig.some((extension) => {
-        return uri.indexOf(extension) > 0
-    })
-
+    return containsExtension(uri)
 }
