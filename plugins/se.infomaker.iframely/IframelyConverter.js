21181a3c0797d57b849abccecca7ea3cd2c23a22
Christoffer Ekberg <christoffer.ekberg@infomaker.se>
2017-10-17 10:41:46 +0200

    SLMWRI-6: Iframely plugin: persist title on save

--- plugins/se.infomaker.iframely/IframelyConverter.js
+++ plugins/se.infomaker.iframely/IframelyConverter.js
@@ -10,10 +10,12 @@ const IframelyConverter = {
     import: function(el, node) {
         if (el.attr('uuid')) { node.uuid = el.attr('uuid') }
 
+        const titleElem = el.find('title')
         const embedCodeElem = el.find('embedCode')
 
         node.dataType = el.attr('type')
         node.url = el.attr('url')
+        node.title = titleElem.text()
         node.embedCode = embedCodeElem.text()
     },
 
@@ -21,15 +23,17 @@ const IframelyConverter = {
     export: function(node, el, converter) {
         const $$ = converter.$$
         const dataElem = $$('data')
+        const titleElem = $$('title')
         const embedCodeElem = $$('embedCode')
 
         if (node.uuid) { el.attr('uuid', node.uuid) }
         el.attr('type', node.dataType)
         el.attr('url', node.url)
 
+        titleElem.innerHTML = node.title
         embedCodeElem.innerHTML = '<![CDATA[' + node.embedCode.replace(']]>', ']]&gt;') + ']]>'
 
-        dataElem.append(embedCodeElem)
+        dataElem.append([titleElem, embedCodeElem])
         el.append(dataElem)
     }
 }
