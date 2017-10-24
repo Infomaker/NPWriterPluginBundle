c021ffc5f1f796de1dd6138621d11a7dd2f019aa
Oscar Karlsson <kridwick@gmail.com>
2017-10-24 17:02:29 +0200

    WPLUG-150 ImageGallery's InsertImageCommand should now be able to handle images from all sources, needs cleanup

--- plugins/se.infomaker.imagegallery/InsertImageCommand.js
+++ plugins/se.infomaker.imagegallery/InsertImageCommand.js
@@ -1,3 +1,4 @@
+import {documentHelpers} from 'substance'
 import {WriterCommand, api, idGenerator} from 'writer'
 
 class InsertImageCommand extends WriterCommand {
@@ -17,40 +18,156 @@ class InsertImageCommand extends WriterCommand {
     handleInsert(tx, params) {
         const imageGalleryNode = params.context.node
         const {data} = params
-        const {files} = data
-        const imageFiles = []
+
+        switch(data.type) {
+            case 'file':
+                this._insertFiles(tx, data.files, imageGalleryNode)
+                break
+            case 'node':
+                this._insertNode(tx, data.nodeId, imageGalleryNode)
+                break
+            case 'uri':
+                this._insertUri(tx, data.uriData, imageGalleryNode)
+                break
+            case 'url':
+                this._insertUrl(tx, data.url, imageGalleryNode)
+                break
+            default:
+                break
+        }
+    }
+
+    /**
+     *
+     * @param tx
+     * @param files
+     * @param node
+     * @private
+     */
+    _insertFiles(tx, files, node) {
         const imageGalleryImages = []
-        const imageId = idGenerator()
-        
-        files.forEach(file => {
-            let imageFile = tx.create({
+
+        files.forEach((file) => {
+            const imageId = idGenerator()
+            const imageGalleryImage = tx.create({
+                parentNodeId: node.id,
+                type: 'imagegalleryimage'
+            })
+            const imageFile = tx.create({
                 id: imageId,
-                parentNodeId: imageGalleryNode.id,
+                parentNodeId: imageGalleryImage.id,
                 type: 'npfile',
                 imType: 'x-im/image',
                 mimeType: file.type,
                 sourceFile: file
             })
-            let imageGalleryImage = tx.create({
-                id: `${imageId}-galleryImage`,
-                parentNodeId: imageGalleryNode.id,
-                type: 'imagegalleryimage',
-            })
-            imageFiles.push(imageFile.id)
+
             imageGalleryImages.push(imageGalleryImage.id)
+            tx.set([imageGalleryImage.id, 'imageFile'], imageFile.id)
         })
 
-        tx.set([imageGalleryNode.id, 'imageFiles'], [...imageGalleryNode.imageFiles, ...imageFiles])
-        tx.set([imageGalleryNode.id, 'nodes'], [...imageGalleryNode.nodes, ...imageGalleryImages])
+        tx.set([node.id, 'nodes'], [...node.nodes, ...imageGalleryImages])
 
         setTimeout(() => {
-            debugger
-            tx.setSelection({
-                type: 'node',
-                path: imageGalleryNode.document.get(imageGalleryNode.id).getContentPath(),
-                startOffset: 0,
-                surfaceId: imageGalleryNode.id
-            })
+            api.editorSession.fileManager.sync()
+        }, 300)
+    }
+
+    /**
+     *
+     * @param tx
+     * @param draggedNodeId
+     * @param imageGalleryNode
+     * @private
+     */
+    _insertNode(tx, draggedNodeId, imageGalleryNode) {
+        try {
+            const doc = api.editorSession.getDocument()
+            const draggedNode = doc.get(draggedNodeId)
+            if (draggedNode && draggedNode.type === 'ximimage') {
+
+                const imageFile = draggedNode.imageFile
+                const imageNode = doc.get(imageFile)
+                const newFileNode = documentHelpers.copyNode(imageNode)[0]
+                newFileNode.parentNodeId = imageGalleryNode.id
+
+                delete newFileNode.id
+
+                let imageFileNode = tx.create(newFileNode)
+
+                const galleryImageNode = tx.create({
+                    parentNodeId: imageGalleryNode.id,
+                    type: 'imagegalleryimage',
+                    imageFile: imageFileNode.id
+                })
+
+                tx.set([imageGalleryNode.id, 'nodes'], [...imageGalleryNode.nodes, galleryImageNode.id])
+            }
+        } catch (_) {
+
+        }
+    }
+
+    /**
+     *
+     * @param tx
+     * @param uriData
+     * @param imageGalleryNode
+     * @private
+     */
+    _insertUri(tx, uriData, imageGalleryNode) {
+        const uuid = uriData.uuid
+
+        if (!uriData.uuid) {
+            throw new Error('Unsupported data. UUID must exist')
+        }
+
+        const imageFileNode = {
+            parentNodeId: imageGalleryNode.id,
+            type: 'npfile',
+            imType: 'x-im/image',
+            uuid: uuid,
+            sourceUUID: uuid,
+        }
+
+        // Create file node for the image
+        let imageFile = tx.create(imageFileNode)
+
+        const galleryImageNode = tx.create({
+            parentNodeId: imageGalleryNode.id,
+            type: 'imagegalleryimage',
+            imageFile: imageFile.id
+        })
+
+        tx.set([imageGalleryNode.id, 'nodes'], [...imageGalleryNode.nodes, galleryImageNode.id])
+    }
+
+    /**
+     * @param tx
+     * @param url
+     * @param imageGalleryNode
+     * @private
+     */
+    _insertUrl(tx, url, imageGalleryNode) {
+        const imageFileNode = {
+            parentNodeId: imageGalleryNode.id,
+            type: 'npfile',
+            imType: 'x-im/image',
+            sourceUrl: url
+        }
+
+        // Create file node for the image
+        let imageFile = tx.create(imageFileNode)
+
+        const galleryImageNode = tx.create({
+            parentNodeId: imageGalleryNode.id,
+            type: 'imagegalleryimage',
+            imageFile: imageFile.id
+        })
+
+        tx.set([imageGalleryNode.id, 'nodes'], [...imageGalleryNode.nodes, galleryImageNode.id])
+
+        setTimeout(() => {
             api.editorSession.fileManager.sync()
         }, 300)
     }
