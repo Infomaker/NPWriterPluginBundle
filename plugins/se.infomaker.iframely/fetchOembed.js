eda42f1069b0018fcd1c4617c25d7a70451495e3
Christoffer Ekberg <christoffer.ekberg@infomaker.se>
2017-11-15 10:21:26 +0100

    WPLUG-172 Add omitScript setting

--- plugins/se.infomaker.iframely/fetchOembed.js
+++ plugins/se.infomaker.iframely/fetchOembed.js
@@ -4,7 +4,12 @@ import fetchWriterIframe from './fetchWriterIframe'
 export default function fetchOembed(fetchUrl) {
     const baseUrl = 'http://iframe.ly/api/oembed'
     const apiKey = api.getConfigValue('se.infomaker.iframely', 'apiKey')
-    const url = `${baseUrl}/?url=${encodeURIComponent(fetchUrl)}&api_key=${apiKey}`
+    const omitScript = api.getConfigValue('se.infomaker.iframely', 'apiKey', false)
+    let url = `${baseUrl}/?url=${encodeURIComponent(fetchUrl)}&api_key=${apiKey}`
+
+    if (omitScript) {
+        url += '&omit_script=1'
+    }
 
     return api.router.get('/api/resourceproxy', { url })
         .then(res => api.router.checkForOKStatus(res))
