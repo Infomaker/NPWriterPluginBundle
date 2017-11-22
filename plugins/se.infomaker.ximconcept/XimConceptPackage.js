a9e87717d25c2d81719e6d3e9e4ee6cfb1243b31
Peter Persson <peter.persson@infomaker.se>
2017-11-22 15:07:17 +0100

    WPLUG-160: Added basic Concept plugin files

---
+++ plugins/se.infomaker.ximconcept/XimConceptPackage.js
@@ -0,0 +1,29 @@
+import ConceptMainComponent from './components/ConceptMainComponent'
+import './scss/xmimconcept.scss'
+
+const XimConceptPackage = {
+    
+    id: 'se.infomaker.ximconcept',
+    name: 'ximconcept',
+    version: '{{version}}',
+    configure(configurator, config) {
+        configurator.addComponentToSidebarWithTabId(
+            config.id,
+            config.tabid || 'main',
+            ConceptMainComponent,
+            config
+        )
+
+        configurator.addLabel('create', {
+            sv: 'Skapa'
+        })
+        configurator.addLabel('save', {
+            sv: 'Spara'
+        })
+        configurator.addLabel('cancel', {
+            sv: 'Avbryt'
+        })
+    }
+}
+
+export default XimConceptPackage
