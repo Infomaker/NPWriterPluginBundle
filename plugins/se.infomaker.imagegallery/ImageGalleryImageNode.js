5b60d743b8192568603c503a031ec3255b50ae42
Oscar Karlsson <kridwick@gmail.com>
2017-11-30 11:15:16 +0100

    WPLUG-193 add crops support to ImageGalleryImageNode

--- plugins/se.infomaker.imagegallery/ImageGalleryImageNode.js
+++ plugins/se.infomaker.imagegallery/ImageGalleryImageNode.js
@@ -1,14 +1,15 @@
 import {withTraits, traitBundle} from 'writer'
 import {BlockNode} from 'substance'
 
-const {imageNodeTrait, authorNodeTrait} = traitBundle
+const {imageNodeTrait, authorNodeTrait, imageCropTrait} = traitBundle
 
 /**
  * @class ImageGalleryImageNode
  * @mixes imageNodeTrait
  * @mixes authorNodeTrait
+ * @mixes imageCropTrait
  */
-class ImageGalleryImageNode extends withTraits(BlockNode, imageNodeTrait, authorNodeTrait) {
+class ImageGalleryImageNode extends withTraits(BlockNode, imageNodeTrait, authorNodeTrait, imageCropTrait) {
 }
 
 ImageGalleryImageNode.define({
@@ -17,7 +18,9 @@ ImageGalleryImageNode.define({
     authors: {type: 'array', default: []},
     caption: {type: 'string', default: ''},
     width: {type: 'number', optional: true},
-    height: {type: 'number', optional: true}
+    height: {type: 'number', optional: true},
+    disableAutomaticCrop: {type: 'boolean', optional: true, default: false},
+    crops: {type: 'object', default: []}
 })
 
 export default ImageGalleryImageNode
