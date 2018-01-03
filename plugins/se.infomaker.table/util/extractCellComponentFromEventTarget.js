b84a1fff0e53384ca5ca84a8c9d5078401cf0232
Christoffer Ekberg <christoffer.ekberg@infomaker.se>
2018-01-03 08:54:11 +0100

    WPLUG-207 Improve cell extraction from event

--- plugins/se.infomaker.table/util/extractCellComponentFromEventTarget.js
+++ plugins/se.infomaker.table/util/extractCellComponentFromEventTarget.js
@@ -2,18 +2,17 @@ import { Component } from 'substance'
 
 export default function(target) {
     const comp = Component.unwrap(target)
+    return findClosestCellParent(comp)
+}
 
+function findClosestCellParent(comp) {
     if (comp) {
         if (comp._isTableCellComponent) {
             return comp
-        } else if (comp._isTextPropertyEditor) {
-            return comp.getParent()
-        } else if (comp._isTextPropertyComponent) {
-            return comp.getParent().getParent()
-        } else {
-            return null
+        } else if (comp.getParent) {
+            const candidate = comp.getParent()
+            return findClosestCellParent(candidate)
         }
     }
-
     return null
 }
