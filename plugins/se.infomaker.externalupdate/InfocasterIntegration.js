2e5073a95c8f85483e169fb17289761eea40b95a
Tobias Södergren <tobias.sodergren@infomaker.se>
2018-06-08 15:52:01 +0200

    WPLUG-263: Working against prod infocaster

--- plugins/se.infomaker.externalupdate/InfocasterIntegration.js
+++ plugins/se.infomaker.externalupdate/InfocasterIntegration.js
@@ -2,7 +2,7 @@ import {infocaster} from 'writer'
 
 class InfocasterIntegration {
     constructor({uuid, callback, token, publisherId}) {
-        const infoCasterClient = new infocaster.WebSocketClient('https://infocaster-stage.lcc.infomaker.io', token);
+        const infoCasterClient = new infocaster.WebSocketClient('https://infocaster.lcc.infomaker.io', token);
 
         infoCasterClient.on('sessionInit', (data) => {
             this.sessionInit = data
