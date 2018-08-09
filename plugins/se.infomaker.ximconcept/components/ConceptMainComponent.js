7c8238afb03555f49f5aa0a7594eaf077c5f084e
Peter Persson <peter.persson@infomaker.se>
2018-08-09 10:11:45 +0200

    Fixed typo

--- plugins/se.infomaker.ximconcept/components/ConceptMainComponent.js
+++ plugins/se.infomaker.ximconcept/components/ConceptMainComponent.js
@@ -69,9 +69,9 @@ class ConceptMainComponent extends Component {
                     this.reloadArticleConcepts()
                 } else if (associatedWith.length && associatedWith === eventName) {
                     const { pluginConfig } = this.state
-                    const associatedLinkes = ConceptService.getArticleConceptsByType(pluginConfig.associatedWith)
+                    const associatedLinks = ConceptService.getArticleConceptsByType(pluginConfig.associatedWith)
 
-                    this.extendState({ associatedLinkes })
+                    this.extendState({ associatedLinks })
                 }
             }
         })
@@ -100,9 +100,9 @@ class ConceptMainComponent extends Component {
     reloadArticleConcepts() {
         const { pluginConfig } = this.state
         const existingItems = ConceptService.getArticleConceptsByType(this.state.conceptType, this.state.types, this.state.subtypes)
-        const associatedLinkes = pluginConfig.associatedWith ? ConceptService.getArticleConceptsByType(pluginConfig.associatedWith) : false
+        const associatedLinks = pluginConfig.associatedWith ? ConceptService.getArticleConceptsByType(pluginConfig.associatedWith) : false
 
-        this.extendState({ existingItems, associatedLinkes })
+        this.extendState({ existingItems, associatedLinks })
     }
 
     getInitialState() {
@@ -113,7 +113,7 @@ class ConceptMainComponent extends Component {
         const subtypes = pluginConfig.subtypes
         const existingItems = ConceptService.getArticleConceptsByType(conceptType, types, subtypes)
         const propertyMap = ConceptService.getPropertyMap()
-        const associatedLinkes = pluginConfig.associatedWith ? ConceptService.getArticleConceptsByType(pluginConfig.associatedWith) : false
+        const associatedLinks = pluginConfig.associatedWith ? ConceptService.getArticleConceptsByType(pluginConfig.associatedWith) : false
 
         return {
             name,
@@ -123,7 +123,7 @@ class ConceptMainComponent extends Component {
             existingItems,
             conceptType,
             propertyMap,
-            associatedLinkes
+            associatedLinks
         }
     }
 
@@ -219,10 +219,10 @@ class ConceptMainComponent extends Component {
     }
 
     shouldBeDisabled() {
-        const { singleValue, pluginConfig, associatedLinkes } = this.state
+        const { singleValue, pluginConfig, associatedLinks } = this.state
 
         return (singleValue && this.state.existingItems.length) ? true :
-            (pluginConfig.associatedWith && (!associatedLinkes || !associatedLinkes.length)) ? true : false
+            (pluginConfig.associatedWith && (!associatedLinks || !associatedLinks.length)) ? true : false
     }
 
     render($$) {
