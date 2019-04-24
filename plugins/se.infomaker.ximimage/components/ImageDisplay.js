bda1febb566aeeed97b4b7587a3f6f6eb3cfb1bd
evespierre <eric.vespierre@newscycle.com>
2019-04-24 09:14:08 -0400

    Prevent multiple clicks on image crop and metadata buttons.

--- plugins/se.infomaker.ximimage/components/ImageDisplay.js
+++ plugins/se.infomaker.ximimage/components/ImageDisplay.js
@@ -12,6 +12,13 @@ class ImageDisplay extends Component {
         e.stopPropagation()
     }
 
+    getInitialState () {
+        return { 
+            cropButtonClicked: false,
+            metadataButtonClicked: false
+        }
+    }
+
     displayCrop(cropUrl) {
         this.extendState({cropUrl: cropUrl});
     }
@@ -24,6 +31,8 @@ class ImageDisplay extends Component {
     }
 
     render($$) {
+        const {cropButtonClicked, metadataButtonClicked} = this.state
+
         let el = $$('div').addClass('sc-image-display')
         let cropOverlay = $$('div').addClass('crop-overlay hidden').ref('cropOverlay')
         let imgContainer = $$('div').addClass('se-image-container checkerboard').ref('imageContainer')
@@ -74,7 +83,15 @@ class ImageDisplay extends Component {
             actionsEl.append(
                 $$(Button, {
                     icon: 'image'
-                }).on('click', this._openMetaData)
+                })
+                .on('click', () => {
+                    // Prevents multiple clicks
+                    if (!metadataButtonClicked) {
+                        this._openMetaData()
+                        this.extendState({metadataButtonClicked: true})
+                    }
+                })
+
             )
         }
 
@@ -110,7 +127,11 @@ class ImageDisplay extends Component {
 
             actionsEl.append($$(Button, {icon: 'crop'})
                 .on('click', () => {
-                    this._openCropper($$)
+                    // Prevents multiple clicks
+                    if (!cropButtonClicked) {
+                        this._openCropper($$)
+                        this.extendState({cropButtonClicked: true})
+                    }
                 })
                 .append($$('em').append(currentCrops).addClass(cropBadgeClass))
             )
@@ -173,6 +194,7 @@ class ImageDisplay extends Component {
                     disableAutomaticCrop: this.props.node.disableAutomaticCrop,
                     abort: () => {
                         this.refs.cropOverlay.addClass('hidden')
+                        this.extendState({cropButtonClicked: false})
                         return true;
                     },
                     restore: () => {
@@ -180,6 +202,7 @@ class ImageDisplay extends Component {
                         if (this.props.notifyCropsChanged) {
                             this.props.notifyCropsChanged()
                         }
+                        this.extendState({cropButtonClicked: false})
                         return false;
                     },
                     save: (newCrops, disableAutomaticCrop) => {
@@ -187,6 +210,7 @@ class ImageDisplay extends Component {
                         if (this.props.notifyCropsChanged) {
                             this.props.notifyCropsChanged()
                         }
+                        this.extendState({cropButtonClicked: false})
                     }
                 })
 
@@ -230,6 +254,7 @@ class ImageDisplay extends Component {
     _openMetaData() {
         api.router.getNewsItem(this.props.node.uuid, 'x-im/image')
             .then(response => {
+                this.extendState({medataButtonClicked: false})
                 api.ui.showDialog(
                     UIDialogImage,
                     {
