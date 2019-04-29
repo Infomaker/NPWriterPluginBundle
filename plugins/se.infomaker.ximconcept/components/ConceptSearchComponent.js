0b6acc39b59e16967cb437b5787511f7386b77e1
Peter Persson <peter.persson@infomaker.se>
2019-04-29 14:44:51 +0200

    WPLUG-499: Improved functionality for "search on focus" configuration

--- plugins/se.infomaker.ximconcept/components/ConceptSearchComponent.js
+++ plugins/se.infomaker.ximconcept/components/ConceptSearchComponent.js
@@ -131,18 +131,25 @@ class ConceptSearchComponent extends Component {
 
     handleFocus() {
         const {searchOnFocus} = this.props
+        let seq = this.state.seq
+        let searching = this.state.searching
 
         this.resetState()
-        this.extendState({
-            searching: this.state.searching + 1,
-            hasFocus: true,
-            seq: this.state.seq + 1
-        })
 
         // Search on focus, unless searchOnFocus is defined and set to false.
         if (searchOnFocus) {
-            this.search('*', this.state.seq)
+            seq = this.state.seq + 1
+            searching = this.state.searching + 1
+
+            this.search('*', seq)
         }
+
+        this.extendState({
+            searching,
+            seq,
+            hasFocus: true,
+        })
+
     }
 
     handleBlur() {
