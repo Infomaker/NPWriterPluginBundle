081d65714f604d8ba0d98191a780a0ffbaafb7e3
Peter Persson <peter.persson@infomaker.se>
2019-09-12 15:05:05 +0200

    WPLUG-563: Migrate se.infomaker.iframely plugin to it’s own repository

--- plugins/se.infomaker.iframely/insertIframelyEmbed.js
+++
@@ -1,9 +0,0 @@
-import Package from './IframelyPackage'
-
-export default function insertIframelyEmbed(tx, url) {
-    tx.insertBlockNode({
-        type: Package.name,
-        dataType: `x-im/${Package.name}`,
-        url: decodeURIComponent(url)
-    })
-}
